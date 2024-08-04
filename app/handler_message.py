34f6b1726a913730a7268f82e58a5231750d0a3b
W1ndys <w1ndys@outlook.com>
2024-08-05 00:08:48 +0800

    增加对不可见字符的过滤

--- app/handler_message.py
+++ app/handler_message.py
@@ -20,22 +20,13 @@ async def handle_message_event(websocket, msg):
     try:
         # 处理群消息
         if msg.get("message_type") == "group":
-
-            # 处理群消息
-            if msg.get("post_type") == "message":
-                group_id = msg["group_id"]
-                logging.info(f"处理群消息,群ID:{group_id}")
-                logging.info(f"原消息内容:{msg}")
-                await handle_group_message(websocket, msg)
-
-                # 编解码功能
-                await handle_crypto_group_message(websocket, msg)
-
-        # 处理群通知
-        if msg.get("post_type") == "notice":
             group_id = msg["group_id"]
-            logging.info(f"处理群通知事件, 群ID: {group_id}")
-            await handle_group_notice(websocket, msg)
+            logging.info(f"处理群消息,群ID:{group_id}")
+            logging.info(f"原消息内容:{msg}")
+            await handle_group_message(websocket, msg)
+
+            # 编解码功能
+            await handle_crypto_group_message(websocket, msg)
 
         # 处理私聊消息
         elif msg.get("message_type") == "private":
@@ -52,7 +43,15 @@ async def handle_message_event(websocket, msg):
 
 # 处理通知事件的逻辑
 async def handle_notice_event(websocket, msg):
-    pass
+
+    # 处理群通知
+    if msg.get("post_type") == "notice":
+        group_id = msg["group_id"]
+        logging.info(f"处理群通知事件, 群ID: {group_id}")
+        if handle_group_notice is not None:
+            await handle_group_notice(websocket, msg)
+        else:
+            logging.error("handle_group_notice is None")
 
 
 # 处理请求事件的逻辑
@@ -73,9 +72,12 @@ async def handle_message(websocket, message):
     logging.info(f"处理消息: {msg}")
 
     if "post_type" in msg:
-        if msg["post_type"] == "message" or msg["post_type"] == "notice":
-            # 处理消息事件，以及通知事件
+        if msg["post_type"] == "message":
+            # 处理消息事件
             await handle_message_event(websocket, msg)
+        elif msg["post_type"] == "notice":
+            # 处理通知事件
+            await handle_notice_event(websocket, msg)
         elif msg["post_type"] == "request":
             # 处理请求事件
             await handle_request_event(websocket, msg)
