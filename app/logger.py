47af46bbf44590a4e8dc769d68805cc020ae0c6f
W1ndys <w1ndys@outlook.com>
2024-08-27 22:35:07 +0800

    修改日志文件，add缓冲区处理，add文件大小新建

--- app/logger.py
+++ app/logger.py
@@ -37,7 +37,10 @@ def setup_logger():
 
     # 添加 RotatingFileHandler 将日志保存到本地文件，并在超过1MB时新建文件
     file_handler = RotatingFileHandler(
-        log_filename, maxBytes=1024 * 1024, backupCount=0, encoding="utf-8"
+        log_filename,
+        maxBytes=1024 * 1024,
+        backupCount=5,
+        encoding="utf-8",  # 修改 backupCount 为 5
     )
     file_handler.setFormatter(
         logging.Formatter(
@@ -55,5 +58,12 @@ def setup_logger():
     root_logger.addHandler(handler)
     root_logger.addHandler(file_handler)
 
+    # 确保日志及时刷新
+    for handler in root_logger.handlers:
+        handler.flush()
+
     logging.info("初始化日志器")
     logging.info(f"日志文件名: {log_filename}")
+
+    # 手动调用 doRollover 确保文件大小超过限制时进行滚动
+    file_handler.doRollover()
