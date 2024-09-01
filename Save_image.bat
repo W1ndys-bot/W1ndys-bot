c2a771175a2ab321147d36a6258a63d14b6b3612
W1ndys <w1ndys@outlook.com>
2024-09-01 17:55:49 +0800

    更新镜像

--- Save_image.bat
+++ Save_image.bat
@@ -1,6 +1,6 @@
 @echo off
 rem 保存 Docker 镜像为 tar 文件
-docker save -o "D:\docker-image\napcat.tar" docker.1panel.dev/mlikiowa/napcat-docker:
+docker save -o "D:\docker-image\napcat.tar" docker.1panel.dev/mlikiowa/napcat-docker
 
 rem 提示用户保存完成
 echo Docker image saved to D:\docker-image\napcat.tar
